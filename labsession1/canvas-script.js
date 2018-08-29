6ce9a96b941f2948fb782d01ddc7a30bdc0b1369
jdiperi88 <jdiperi88@yahoo.com>
2018-08-29 16:40:40 -0400

    practiced making curves

--- labsession1/canvas-script.js
+++ labsession1/canvas-script.js
@@ -2,15 +2,20 @@ window.onload = () => {
 	//definitions
 	var canvas = document.getElementById("canvas");
 	var context = canvas.getContext("2d");
-
-	//Drawing lines
+	var radian = Math.PI / 180;
+	//first arc
 	context.beginPath(); //reset the context state
 	context.strokeStyle = "red";
 	context.lineWidth = 15; //changes thickness of the line
 	context.lineJoin = "round";
-	context.moveTo(60, 80); // moveTo(x,y) -> starting the point of line
-	context.lineTo(160, 80); // line(x,y) -> end point of the line
-	context.lineTo(100, 220);
-	context.lineTo(180, 180);
+	context.arc(100, 80, 50, 0 * radian, 180 * radian, false);
+	context.stroke(); //draws the line
+
+	//second arc
+	context.beginPath(); //reset the context state
+	context.strokeStyle = "green";
+	context.lineWidth = 15; //changes thickness of the line
+	context.lineJoin = "round";
+	context.arc(100, 30, 50, 45 * radian, 320 * radian, false);
 	context.stroke(); //draws the line
 };
