278f2285fed9657f018739140f9a6084f8da25e0
jdiperi88 <jdiperi88@yahoo.com>
2018-09-11 16:18:05 -0400

    drew pacman on canvas

--- circles/canvas-script.js
+++ circles/canvas-script.js
@@ -6,11 +6,26 @@ window.onload = () => {
 
 	//circles
 	// context.arc(centerx,centerY,radius,startAngle, endAngle, antiClockwiseDirection)
+
+	//first way
+
+	//body
 	context.beginPath();
-	context.strokeStyle = "blue";
+	context.strokeStyle = "orange";
 	context.lineWidth = 10;
 	context.fillStyle = "orange";
-	context.arc(450, 300, 150, 0 * radian, 360 * radian, false);
+	context.moveTo(250, 250);
+	context.lineTo(330, 310);
+
+	context.arc(250, 250, 100, 37 * radian, 323 * radian, false);
+	context.lineTo(250, 250);
+	context.stroke();
+	context.fill();
+
+	//eye
+	context.beginPath();
+	context.fillStyle = "black";
+	context.arc(250, 200, 10, 0 * radian, 360 * radian, false);
 	context.stroke();
 	context.fill();
 };
