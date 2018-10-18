d691fa5570903497e3b90d2ea65737d7b3e5188f
jdiperi88 <jdiperi88@yahoo.com>
2018-10-18 11:17:14 -0400

    scaled rectangles

--- transformations/canvas-script.js
+++ transformations/canvas-script.js
@@ -2,4 +2,27 @@ window.onload = function() {
 	//definitions
 	var canvas = document.getElementById("canvas");
 	var context = canvas.getContext("2d");
+	//context.scale(horizontalScaleFactor, verticalScaleFactor, ) -> scales the canvas context
+	//horizontalScaleFactor(double): scales the width of the current drawing
+	//verticalScaleFactor(double): scales the height of the current drawing
+
+	//red rect
+	context.fillStyle = "red";
+	context.fillRect(50, 50, 100, 40);
+	context.fill();
+
+	//scale
+	context.scale(2, 2);
+
+	//scaled version
+	context.fillStyle = "blue";
+	context.fillRect(50, 50, 100, 40);
+	context.fill();
+
+	//scale
+	context.scale(2, 2);
+	//scaled version
+	context.fillStyle = "green";
+	context.fillRect(50, 50, 100, 40);
+	context.fill();
 };
